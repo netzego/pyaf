8bea953fa14f4db849a0080a5b16fe33325f524b
netzego <github@netzego.de>
2022-05-14 10:32:16 +0200

    feat(tests): Add getter tests for `Vorbis` class

--- tests/test_vorbis.py
+++ tests/test_vorbis.py
@@ -10,3 +10,50 @@ def test_Vorbis__init(testfile):
 def test_Vorbis__attrs():
     assert hasattr(Vorbis, "save")
     assert hasattr(Vorbis, "wipe")
+    assert hasattr(Vorbis, "album")
+    assert hasattr(Vorbis, "albumartist")
+    assert hasattr(Vorbis, "title")
+    assert hasattr(Vorbis, "tracknumber")
+    assert hasattr(Vorbis, "totaltracks")
+    assert hasattr(Vorbis, "discnumber")
+    assert hasattr(Vorbis, "totaldiscs")
+
+
+def test_Vorbis__VorbisCore_getter_album(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.album == "test_album"
+
+
+def test_Vorbis__VorbisCore_getter_albumartist(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.albumartist == "test_albumartist"
+
+
+def test_Vorbis__VorbisCore_getter_artist(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.artist == "test_artist"
+
+
+def test_Vorbis__VorbisCore_getter_title(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.title == "test_title"
+
+
+def test_Vorbis__VorbisCore_getter_tracknumber(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.tracknumber == 1
+
+
+def test_Vorbis__VorbisCore_getter_totaltracks(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.totaltracks == 1
+
+
+def test_Vorbis__VorbisCore_getter_discnumber(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.discnumber == 1
+
+
+def test_Vorbis__VorbisCore_getter_totaldiscs(testfile):
+    vorbis = Vorbis(FLAC(testfile("vorbis/vorbis.flac")))
+    assert vorbis.totaldiscs == 1
